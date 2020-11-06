775090623f6ce383c356a8ec162af6a4ace63b3c
Drew Thompson <drewallynthompson@gmail.com>
2020-11-06 03:53:57 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,8 +24,10 @@ class ApplicationController < Sinatra::Base
   
   get '/articles/:id' do
     @article = Article.find(params[:id])
-
     erb :show
+  end
+  
+  get '/articles/:id/edit' do
   end
 
 end
