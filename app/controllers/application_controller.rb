ed64d09c3db98f17df0865c60e59616de926ee99
Drew Thompson <drewallynthompson@gmail.com>
2020-11-06 03:19:55 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,20 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-
-  get '/' do
+  
+  get '/articles' do
+    @articles = Article.all 
+    erb :index
   end
+  
+  get '/articles/new' do
+    erb :new
+  end
+  
+  post '/articles' do
+    @article = Article.create(title: params[:title], content: params[:content] )
+    redirect "/articles/#{Article.last.id}"
+  end
+
 end
+
