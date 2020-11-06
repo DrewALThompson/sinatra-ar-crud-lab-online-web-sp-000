08d30257684499c8923a3ca1c091c5ee26da2be8
Drew Thompson <drewallynthompson@gmail.com>
2020-11-06 04:17:14 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -34,7 +34,7 @@ class ApplicationController < Sinatra::Base
   
   patch '/articles/:id' do 
     @article = Article.find(params[:id])
-    @article.update(params[:title])
+    @article.update(params[:article])
     redirect to "/articles/#{@article.id}"
   end
 
